e8c7a67e2dd5909d487c380a58471a3c05bea0c1
xjcaa <andrewchen67@gmail.com>
2021-12-23 00:33:49 +0800

    Fix floating point error on `convertDecimalToNativeInteger`. (#43)

--- src/utils.ts
+++ src/utils.ts
@@ -396,7 +396,9 @@ export function sortMarketKeys(keys: PublicKey[]): PublicKey[] {
  * Converts a decimal number to native fixed point integer of precision 6.
  */
 export function convertDecimalToNativeInteger(amount: number): number {
-  return Math.floor(amount * Math.pow(10, constants.PLATFORM_PRECISION));
+  return parseInt(
+    (amount * Math.pow(10, constants.PLATFORM_PRECISION)).toFixed(0)
+  );
 }
 
 /**
@@ -438,7 +440,9 @@ export function convertNativeLotSizeToDecimal(amount: number): number {
  * @param amount
  */
 export function convertDecimalToNativeLotSize(amount: number): number {
-  return amount * Math.pow(10, constants.POSITION_PRECISION);
+  return parseInt(
+    (amount * Math.pow(10, constants.POSITION_PRECISION)).toFixed(0)
+  );
 }
 
 export async function getTokenMint(
