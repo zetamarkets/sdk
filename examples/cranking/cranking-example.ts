26c6eb3748ce0d4d5d90601045c3c99eb0df9ea0
xjcaa <andrewchen67@gmail.com>
2021-12-26 17:06:20 +0800

    Fixup examples

--- examples/cranking/cranking-example.ts
+++ examples/cranking/cranking-example.ts
@@ -7,6 +7,7 @@ import {
   utils,
   programTypes,
   constants,
+  Market,
 } from "@zetamarkets/sdk";
 import { PublicKey, Connection, Keypair } from "@solana/web3.js";
 
@@ -15,10 +16,20 @@ const PROGRAM_ID = new PublicKey(process.env["program_id"]);
 let crankingMarkets = new Array(constants.ACTIVE_MARKETS).fill(false);
 console.log(NETWORK_URL);
 
+export async function sleep(ms: number) {
+  await new Promise((resolve) => setTimeout(resolve, ms, undefined));
+}
+
 async function main() {
   // Generate a new keypair for wallet otherwise load from a private key.
   const userKey = Keypair.generate();
   const wallet = new Wallet(userKey);
+
+  // Load from private_key stored in .env file.
+  // const privateKey = Keypair.fromSecretKey(
+  //  new Uint8Array(JSON.parse(Buffer.from(process.env.private_key).toString()))
+  //);
+  // const wallet = new Wallet(privateKey);
 
   // Create a solana web3 connection to devnet.
   const connection = new Connection(NETWORK_URL, utils.defaultCommitment());
@@ -44,13 +55,22 @@ async function main() {
     },
     process.env.DISPLAY_STATE_INTERVAL
       ? parseInt(process.env.DISPLAY_STATE_INTERVAL)
-      : 5000
+      : 30000
   );
 
   // Crank all markets.
   setInterval(
     async function () {
       await crankExchange(true);
+      // Use this instead of `crankExchange` if you wish to throttle your cranking.
+      /*
+      await crankExchangeThrottled(
+        true,
+        process.env.CRANK_EXCHANGE_THROTTLE_MS
+          ? parseInt(process.env.CRANK_EXCHANGE_THROTTLE_MS)
+          : 1000
+      );
+      */
     },
     process.env.CRANK_EXCHANGE_INTERVAL
       ? parseInt(process.env.CRANK_EXCHANGE_INTERVAL)
@@ -74,7 +94,7 @@ async function main() {
     },
     process.env.RETREAT_MARKET_NODES_INTERVAL
       ? parseInt(process.env.RETREAT_MARKET_NODES_INTERVAL)
-      : 10000
+      : 5000
   );
 
   // Rebalance zeta vault and insurance fund
@@ -89,7 +109,7 @@ async function main() {
 }
 
 /**
- * This calls zeta's permissinless update pricing function through the Exchange object.
+ * This calls zeta's permissionless update pricing function through the Exchange object.
  * Cranks zeta's on-chain pricing ensuring all our greeks and theos are up-to-date.
  */
 async function updatePricing() {
@@ -118,7 +138,7 @@ async function updatePricing() {
 }
 
 /**
- * This calls zeta's permissinless retreat market nodes function through the Exchange object.
+ * This calls zeta's permissionless retreat market nodes function through the Exchange object.
  * Cranks zeta's on-chain volatility, retreat and interest functionality similiar to update pricing.
  */
 async function retreatMarketNodes() {
@@ -141,14 +161,7 @@ async function retreatMarketNodes() {
   );
 }
 
-/**
- * cranks the serum dex event queue for each zeta market. This will process trades that consist of maker fills.
- * All other events are atomically processed at time of call such as taker fills and cancels.
- * Functionality here will keep track of markets that are currently being cranked, markets that have empty event queues
- * as well as allwoing specification of whether only live markets are being cranked.
- * This will flush event queues completely upon call.
- */
-async function crankExchange(liveOnly: boolean) {
+function getMarketsToCrank(liveOnly: boolean): Market[] {
   let marketsToCrank = [];
   if (liveOnly) {
     let liveExpiryIndices = Exchange.markets.getTradeableExpiryIndices();
@@ -159,6 +172,20 @@ async function crankExchange(liveOnly: boolean) {
   } else {
     marketsToCrank = Exchange.markets.markets;
   }
+  return marketsToCrank;
+}
+
+/**
+ * Cranks the serum dex event queue for each zeta market. This will process trades that consist of maker fills.
+ * All other events are atomically processed at time of call such as taker fills and cancels.
+ * Functionality here will keep track of markets that are currently being cranked, markets that have empty event queues
+ * as well as allowing specification of whether only live markets are being cranked.
+ * This will flush event queues completely upon call.
+ * This function will poll all market event queues asynchronously so is quite expensive in terms of RPC requests per second.
+ * Use crankExchangeThrottle if you are running into rate limit issues.
+ */
+async function crankExchange(liveOnly: boolean) {
+  let marketsToCrank: Market[] = getMarketsToCrank(liveOnly);
   marketsToCrank.map(async (market) => {
     let eventQueue = await market.serumMarket.loadEventQueue(
       Exchange.provider.connection
@@ -196,6 +223,49 @@ async function crankExchange(liveOnly: boolean) {
 }
 
 /**
+ * Iteratively cranks each market event queue.
+ * Allows an optional argument for `throttleMs` which is the duration it will sleep after each market crank.
+ */
+async function crankExchangeThrottled(liveOnly: boolean, throttleMs: number) {
+  let marketsToCrank: Market[] = getMarketsToCrank(liveOnly);
+  for (var i = 0; i < marketsToCrank.length; i++) {
+    let market = marketsToCrank[i];
+    let eventQueue = await market.serumMarket.loadEventQueue(
+      Exchange.provider.connection
+    );
+    if (eventQueue.length > 0 && !crankingMarkets[market.marketIndex]) {
+      crankingMarkets[market.marketIndex] = true;
+      try {
+        while (eventQueue.length != 0) {
+          try {
+            await utils.crankMarket(market.marketIndex);
+          } catch (e) {
+            console.error(
+              `Cranking failed on market ${market.marketIndex}, ${e}`
+            );
+          }
+
+          let currLength = eventQueue.length;
+
+          eventQueue = await market.serumMarket.loadEventQueue(
+            Exchange.provider.connection
+          );
+
+          let numCranked = currLength - eventQueue.length;
+          console.log(
+            `Cranked ${numCranked} events for market ${market.marketIndex}`
+          );
+        }
+      } catch (e) {
+        console.error(`${e}`);
+      }
+      crankingMarkets[market.marketIndex] = false;
+      await sleep(throttleMs);
+    }
+  }
+}
+
+/**
  * Rebalances the zeta vault and the insurance vault to ensure consistent platform security.
  * Checks all margin accounts for non-zero rebalance amounts and rebalances them all.
  */
@@ -218,7 +288,7 @@ async function rebalanceInsuranceVault() {
   try {
     await Exchange.rebalanceInsuranceVault(remainingAccounts);
   } catch (e) {
-    this.alert("Rebalance insurance vault error on transaction!", true);
+    console.error("Rebalance insurance vault error on transaction!");
   }
 }
 
