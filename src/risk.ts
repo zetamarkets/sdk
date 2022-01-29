396bd2daa58622bf1fe0b8ed2b128eae7c4cca2f
xjcaa <andrewchen67@gmail.com>
2022-01-29 17:46:31 +0800

    tmp

--- src/risk.ts
+++ src/risk.ts
@@ -152,7 +152,7 @@ export class RiskCalculator {
         this.getMarginRequirement(
           i,
           // Negative for sells.
-          convertNativeLotSizeToDecimal(-position.openingOrders[1]),
+          -convertNativeLotSizeToDecimal(position.openingOrders[1].toNumber()),
           MarginType.INITIAL
         ) +
         this.getMarginRequirement(
