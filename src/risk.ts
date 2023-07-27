2fceeb76865ecf93ee8ac48a446f066abda99052
Filip <filip@sierra.team>
2023-07-27 12:48:29 +1000

    revert

--- src/risk.ts
+++ src/risk.ts
@@ -218,24 +218,24 @@ export class RiskCalculator {
         continue;
       }
 
-      let price = executionPrice
-        ? executionPrice
-        : Exchange.getMarkPrice(asset);
+      let subExchange = Exchange.getSubExchange(asset);
       if (size > 0) {
         assetPnl +=
-          convertNativeLotSizeToDecimal(size) * price -
+          convertNativeLotSizeToDecimal(size) *
+            (executionPrice ? executionPrice : subExchange.getMarkPrice()) -
           convertNativeBNToDecimal(position.costOfTrades);
       } else {
         assetPnl +=
-          convertNativeLotSizeToDecimal(size) * price +
+          convertNativeLotSizeToDecimal(size) *
+            (executionPrice ? executionPrice : subExchange.getMarkPrice()) +
           convertNativeBNToDecimal(position.costOfTrades);
       }
       if (addTakerFees) {
         assetPnl -=
           convertNativeLotSizeToDecimal(Math.abs(size)) *
-          price *
           (convertNativeBNToDecimal(Exchange.state.nativeD1TradeFeePercentage) /
-            100);
+            100) *
+          subExchange.getMarkPrice();
       }
       upnlMap.set(asset, assetPnl);
       pnl += assetPnl;
