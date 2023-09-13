030582b788f7f56979cd1e4d2da3b90f5bc98e5b
Filip <filip@sierra.team>
2023-09-13 13:05:21 +1000

    exchange event

--- src/events.ts
+++ src/events.ts
@@ -1,5 +1,9 @@
 export enum EventType {
   /**
+   * Refers to events that reflect a change in the exchange state.
+   */
+  EXCHANGE,
+  /**
    * Expiration event for a zeta group.
    */
   EXPIRY,
@@ -41,6 +45,8 @@ export interface OrderbookEvent {
 
 export function eventTypeToString(event: EventType) {
   switch (event) {
+    case EventType.EXCHANGE:
+      return "EXCHANGE";
     case EventType.EXPIRY:
       return "EXPIRY";
     case EventType.USER:
