c47e94cc411c90d123bfa00f61a26d01d4eb8673
Zeta dev <dmitry@sierra.team>
2023-12-14 13:42:53 +0200

    Push orderbook subscription

--- examples/subscription/subscription.ts
+++ examples/subscription/subscription.ts
@@ -8,9 +8,11 @@ import {
   programTypes,
   subscription,
   constants,
+  CrossClient,
 } from "@zetamarkets/sdk";
 
 import { PublicKey, Connection } from "@solana/web3.js";
+import { Asset } from "@zetamarkets/sdk/dist/constants";
 
 const NETWORK_URL = process.env["network_url"]!;
 
@@ -34,31 +36,38 @@ async function main() {
   // Create a solana web3 connection to devnet.
   const connection = new Connection(NETWORK_URL, "confirmed");
 
-  const loadExchangeConfig = types.defaultLoadExchangeConfig(
-    network,
+  const LOAD_CONFIG = {
     connection,
-    utils.defaultCommitment(),
-    0, // ThrottleMs - increase if you are running into rate limit issues on startup.
-    true
-  );
+    opts: utils.commitmentConfig('processed'),
+    network,
+    throttleMs: 0,
+    loadFromStore: true,
+    // A really high amount, so the midpoint doesn't go whack if rpcs breaks
+    // see: https://sierra-mountain.slack.com/archives/C046V1K4ZM2/p1700576227100309
+    TIFBufferSeconds: 10000,
+  };
 
   await Exchange.load(
-    loadExchangeConfig,
+    LOAD_CONFIG,
     // Exchange wallet can be ignored for normal clients.
     undefined
   );
 
-  subscription.subscribeProgramAccounts<programTypes.MarginAccount>(
-    constants.Asset.SOL,
-    types.ProgramAccountType.MarginAccount,
-    async (
-      data: subscription.AccountSubscriptionData<programTypes.MarginAccount>
-    ) => {
-      console.log(data);
-    }
-  );
+  while (!Exchange.isInitialized) {
+    await utils.sleep(100);
+  }
 
-  await utils.sleep(100_000);
+  Exchange.getPerpMarket(Asset.SOL).subscribeOrderbook(() => console.log('Orderbook updated'));
+
+
+  while (true) {
+    const orderbook = Exchange.getOrderbook(Asset.SOL);
+    console.log("Orderbook", `${orderbook.bids[0]?.price} @ ${orderbook.bids[0]?.size}`, `${orderbook.asks[0]?.price} @ ${orderbook.asks[0]?.size}`);
+    
+    await utils.sleep(300);
+  }
+
+ 
 
   // Close to end the websockets.
   // await Exchange.close();
