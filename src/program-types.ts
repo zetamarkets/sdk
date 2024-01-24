a566ec158d2795c65a2fc42e5476c7da1587d7c1
Filip <filip@sierra.team>
2024-01-24 13:39:56 +1100

    sdk types

--- src/program-types.ts
+++ src/program-types.ts
@@ -385,6 +385,7 @@ export interface TradeEventV3 {
   sequenceNumber: anchor.BN; // Unique id for the given market
   fee: anchor.BN;
   price: anchor.BN;
+  pnl: anchor.BN;
 }
 
 export interface PositionMovementEvent {
@@ -431,4 +432,5 @@ export interface ApplyFundingEvent {
   remainingBalance: anchor.BN;
   fundingRate: anchor.BN;
   oraclePrice: anchor.BN;
+  positionSize: anchor.BN;
 }
